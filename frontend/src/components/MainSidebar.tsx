6960572b7923401385bd613a557e1851e1345a69
Atechchatterjee <atechchatterjeee@gmail.com>
2024-07-09 15:07:46 +0530

    feat: add link for desktop & decrease font size

--- frontend/src/components/MainSidebar.tsx
+++ frontend/src/components/MainSidebar.tsx
@@ -1,8 +1,160 @@
-export default function MainSidebar() {
+import { RootContext } from "@/context";
+import { cn } from "@/lib/utils";
+import {
+  RemixiconComponentType,
+  RiComputerFill,
+  RiComputerLine,
+  RiDownload2Fill,
+  RiDownload2Line,
+  RiFileList2Fill,
+  RiFileList2Line,
+  RiFolderAddFill,
+  RiHome2Fill,
+  RiHome2Line,
+  RiImageFill,
+  RiImageLine,
+  RiMenuFill,
+  RiStarFill,
+  RiStarLine,
+  RiToolsFill,
+} from "@remixicon/react";
+import { useContext } from "react";
+import { useState } from "react";
+import NewFileDialog from "./NewFileDialog";
+import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Separator } from "./ui/separator";
+
+type NavLink = {
+  text: "Home" | "Documents" | "Starred" | "Downloads" | "Pictures" | "Desktop";
+  icon: RemixiconComponentType;
+  filledIcon?: RemixiconComponentType;
+  href: string;
+  active?: boolean;
+};
+
+function NavItems({
+  icon: Icon,
+  text,
+  filledIcon: FilledIcon,
+  active = false,
+  className,
+  ...props
+}: NavLink & React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="flex flex-col h-[100vh] min-w-[17rem] bg-[#181B21]">
-      <section className="section-1 px-4 py-6 flex">
-        <p className="text-white font-Geist font-semibold">Kosh</p>
+    <div
+      className={cn(
+        "flex gap-3 items-center py-2 px-3",
+        active
+          ? "bg-white text-black hover:bg-white/90"
+          : "bg-transparent  hover:bg-[#2d333e]",
+        "cursor-pointer rounded-lg",
+        className,
+      )}
+      {...props}
+    >
+      {active ? !!FilledIcon && <FilledIcon size={18} /> : <Icon size={18} />}
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default function MainSidebar({ cb }: { cb?: (_: string) => void }) {
+  const { homeDir, getFileList } = useContext(RootContext);
+  const NavLinks: NavLink[] = [
+    {
+      text: "Starred",
+      icon: RiStarLine,
+      filledIcon: RiStarFill,
+      href: homeDir,
+    },
+    {
+      text: "Home",
+      icon: RiHome2Line,
+      filledIcon: RiHome2Fill,
+      href: homeDir,
+    },
+    {
+      text: "Documents",
+      icon: RiFileList2Line,
+      filledIcon: RiFileList2Fill,
+      href: `${homeDir}/Documents`,
+    },
+    {
+      text: "Downloads",
+      icon: RiDownload2Line,
+      filledIcon: RiDownload2Fill,
+      href: `${homeDir}/Downloads`,
+    },
+    {
+      text: "Desktop",
+      icon: RiComputerLine,
+      filledIcon: RiComputerFill,
+      href: `${homeDir}/Desktop`,
+    },
+    {
+      text: "Pictures",
+      icon: RiImageLine,
+      filledIcon: RiImageFill,
+      href: `${homeDir}/Pictures`,
+    },
+  ];
+  const [activeLink, setActiveLink] =
+    useState<(typeof NavLinks)[number]["text"]>("Home");
+  const [popoverOpen, setPopOverOpen] = useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col h-[100vh] min-w-[15rem] bg-[#181B21] select-none text-sm">
+      <section className="flex px-4 pt-1 pb-1 items-center">
+        <div className="flex gap-4 items-center">
+          <img src="/src/assets/icon-light-wbg.svg" className="h-[1.2rem]" />
+          <p className="text-white text-sm font-Geist font-semibold h-fit mt-[0.3rem]">
+            Kosh
+          </p>
+        </div>
+        <Popover
+          open={popoverOpen}
+          onOpenChange={() => setPopOverOpen(!popoverOpen)}
+        >
+          <PopoverTrigger className="ml-auto text-white">
+            <Button variant="link" className="text-white">
+              <RiMenuFill size={18} />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-1 w-[10rem]">
+            <NewFileDialog
+              className="flex w-full"
+              triggerButton={() => (
+                <div className="flex gap-1 px-2 w-full items-center cursor-pointer rounded-md hover:bg-slate-200">
+                  <RiFolderAddFill size={18} />
+                  <div className="py-2 px-2 text-sm">New Folder</div>
+                </div>
+              )}
+              successCb={() => {
+                setPopOverOpen(false);
+                getFileList();
+              }}
+            />
+            <div className="flex gap-1 px-2 items-center cursor-pointer rounded-md hover:bg-slate-200">
+              <RiToolsFill size={18} />
+              <div className="py-2 px-2 text-sm">Preferences</div>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </section>
+      <Separator className="w-[90%] mx-auto" />
+      <section className="flex flex-col gap-2 text-white px-3 mt-[1rem]">
+        {NavLinks.map((navLink, i) => (
+          <NavItems
+            key={i}
+            {...navLink}
+            active={activeLink === navLink.text}
+            onClick={() => {
+              setActiveLink(navLink.text);
+              if (cb) cb(navLink.href);
+            }}
+          />
+        ))}
       </section>
     </div>
   );

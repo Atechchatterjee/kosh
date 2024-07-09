c990b7e327c5d87573d8506826143cc9d401484d
Atechchatterjee <atechchatterjeee@gmail.com>
2024-07-09 15:06:06 +0530

    feat: add dialog component for creating new folder

---
+++ frontend/src/components/NewFileDialog.tsx
@@ -0,0 +1,82 @@
+import { RootContext } from "@/context";
+import { removeTrailingSlash } from "@/lib/utils";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { createContext, useContext } from "react";
+import { useState } from "react";
+import { CreateDir } from "../../wailsjs/go/main/App";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+
+export const NewFileDialogContext = createContext<{
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}>({ open: false, setOpen: () => {} });
+
+export function NewFileDialogStateWrapper({
+  children,
+}: React.PropsWithChildren) {
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <NewFileDialogContext.Provider value={{ open, setOpen }}>
+      {children}
+    </NewFileDialogContext.Provider>
+  );
+}
+
+export default function NewFileDialog({
+  triggerButton: TriggerButton,
+  successCb,
+  className,
+  ...props
+}: {
+  successCb?: () => void;
+  triggerButton?: React.FC<{}>;
+  asHook?: boolean;
+} & React.HTMLAttributes<HTMLButtonElement>) {
+  const { open, setOpen } = useContext(NewFileDialogContext);
+  const [newFolderName, setNewFolderName] = useState<string>("");
+  const { filePath } = useContext(RootContext);
+
+  async function handleFolderCreation() {
+    const newFolderPath = `${removeTrailingSlash(filePath)}/${newFolderName}`;
+    try {
+      await CreateDir(newFolderPath);
+      if (successCb) successCb();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <Dialog modal open={open} onOpenChange={setOpen}>
+      <DialogTrigger className={className} {...props}>
+        {!!TriggerButton && <TriggerButton />}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Folder</DialogTitle>
+          <DialogDescription className="pt-4 flex gap-4">
+            <form onSubmit={handleFolderCreation}>
+              <Input
+                placeholder="Folder Name"
+                value={newFolderName}
+                onChange={(e) => setNewFolderName(e.target.value)}
+              />
+              <DialogClose asChild>
+                <Button type="submit">Create</Button>
+              </DialogClose>
+            </form>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
